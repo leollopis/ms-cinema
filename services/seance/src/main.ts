1726c473ed3f160108caf693ada38c364c7fe7e7
Zully <dany.crea0@gmail.com>
2026-01-19 10:25:10 +0100

    seance microservice

---
+++ services/seance/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const port = Number(process.env.PORT || process.env.APP_PORT || 3000);
+  await app.listen(port, '0.0.0.0');
+  console.log(`Listening on port ${port}`);
+}
+bootstrap();
